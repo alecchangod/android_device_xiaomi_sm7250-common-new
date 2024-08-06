0548c6153879e162af079d06676ac3649c01a0c5
Bruno Martins <bgcngm@gmail.com>
2024-08-06 16:45:36 +0530

    sm7250-common: Build fingerprint HAL dependency missed after U QPR3
    Change-Id: I7074078801dceff96ce206bd5c460a2dad36b6c8

--- lito.mk
+++ lito.mk
@@ -225,7 +225,8 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.xiaomi
+    android.hardware.biometrics.fingerprint@2.3-service.xiaomi \
+    liblzma.vendor
 
 ifeq ($(TARGET_HAS_UDFPS),true)
 PRODUCT_PACKAGES += \
