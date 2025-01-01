5f622dcdedcffc0ac1a29fed25af154c25f8c65e
Alec Chan <alecchangod@gmail.com>
2025-01-01 16:36:16 +0800

    sm7250-common: props: fix path

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -164,9 +164,9 @@ BOARD_USES_QCOM_HARDWARE := true
 TARGET_BOARD_PLATFORM := lito
 
 # Properties
-TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
+TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
 TARGET_PRODUCT_PROP += $(COMMON_PATH)/product.prop
-TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
 
 # Recovery
 TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/rootdir/etc/fstab.qcom
